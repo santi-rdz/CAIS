846d3c0bd63668f3421bf81f24201634a0767b76
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-03-05 01:05:05 -0800

    test-2 workflow

--- frontend/src/ui/AppLayout.jsx
+++ frontend/src/ui/AppLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router'
 import { Sidebar } from './Sidebar'
 import { Header } from './Header'
-import Button from './Button'
+
 
 export default function AppLayout() {
   return (
     <div className="layout grid h-dvh">
       <Sidebar />
       <Header />
-    
+
       <main className="overflow-scroll bg-gray-50 py-10 [grid-area:main]">
         <div className="my-container">
           <Outlet />
