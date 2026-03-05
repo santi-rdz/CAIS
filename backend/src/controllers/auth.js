f4885f3576d1a458d7330e13ec5fc5b8ad8064af
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-03-04 16:03:32 -0800

    hotfix password comparison

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -29,7 +29,7 @@ export class AuthController {
       }
 
       // Comparar password usando bcrypt
-      const isMatch = password === user.password
+      const isMatch = await bcrypt.compare(password, user.password)
       if (!isMatch) {
         return res.status(401).json({ error: 'Contraseña invalida' })
       }
