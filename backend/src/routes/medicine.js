4643be49fc479002b0efc22e186ccd9b4cca7c28
odabachea2207259 <odabachea2207259@uabc.edu.mx>
2026-03-11 16:01:54 -0700

    Creation of modular routing

---
+++ backend/src/routes/medicine.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { requireAuth } from '../middleware/auth.js'
+import { emergencyRouter } from './medicina/emergencies.js'
+import { evolutionNotesRouter } from './medicina/evolutionNotes.js'
+
+export const medicineRouter = Router()
+
+medicineRouter.use(requireAuth)
+
+medicineRouter.use('/emergencias', emergencyRouter)
+medicineRouter.use('/notas-evolucion', evolutionNotesRouter)
