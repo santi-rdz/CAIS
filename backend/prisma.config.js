5ce0cd78e4546560ea16aedf2451bf18c4cf4fc6
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-03-18 15:19:58 -0700

    added planes_estudio_cie_10 table

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { defineConfig } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
 function makeAdapter() {
@@ -18,7 +18,7 @@ export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: env('DATABASE_URL'),
   },
   migrate: {
     adapter: makeAdapter,
