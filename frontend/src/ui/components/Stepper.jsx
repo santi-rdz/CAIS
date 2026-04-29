c48e744e07bfaea6415c540d00e07d80aea7a422
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-04-29 15:09:22 -0700

    refactor(Stepper): improve responsive layout and sizing

--- frontend/src/ui/components/Stepper.jsx
+++ frontend/src/ui/components/Stepper.jsx
@@ -8,8 +8,10 @@ export default function Stepper({
   className = '',
 }) {
   return (
-    <div className={`overflow-x-auto px-10 py-2 pb-6 max-sm:px-4 ${className}`}>
-      <div className="mx-4 flex min-w-max" style={{ gap }}>
+    <div
+      className={`px-2 py-2 pb-6 max-sm:overflow-x-auto max-sm:px-1 ${className}`}
+    >
+      <div className="mx-4 flex" style={{ gap }}>
         {steps.map((step, i) => {
           const isLast = i === steps.length - 1
           const isCompleted = current > i
@@ -23,19 +25,19 @@ export default function Stepper({
               <div className="relative">
                 <button
                   onClick={() => setCurrStep(i)}
-                  className={`text-5 flex size-6.5 shrink-0 cursor-pointer items-center justify-center rounded-full transition-transform ${isActive ? 'scale-105 ring-2 ring-green-800 ring-offset-2' : ''} ${isActive || isCompleted ? 'bg-green-800 text-white' : 'bg-gray-200 text-neutral-500'}`}
+                  className={`text-6 flex size-6 shrink-0 cursor-pointer items-center justify-center rounded-full transition-transform ${isActive ? 'scale-105 ring-2 ring-green-800 ring-offset-2' : ''} ${isActive || isCompleted ? 'bg-green-800 text-white' : 'bg-gray-200 text-neutral-500'}`}
                 >
                   {isCompleted ? <HiCheck strokeWidth={1} /> : i + 1}
                 </button>
                 <p
-                  className={`text-6 absolute left-1/2 mt-2 w-fit -translate-x-1/2 text-center text-nowrap text-gray-500 ${isActive || isCompleted ? 'text-green-800' : ''}`}
+                  className={`text-6 absolute left-1/2 mt-2 max-w-[10ch] -translate-x-1/2 truncate text-center text-nowrap text-gray-500 ${isActive || isCompleted ? 'text-green-800' : ''}`}
                 >
                   {step}
                 </p>
               </div>
               {!isLast && (
                 <div
-                  className={`h-1 w-full min-w-30 shrink-0 transition-colors duration-500 ease-in-out max-sm:min-w-20 lg:w-full ${isCompleted ? 'bg-green-800' : 'bg-gray-200'}`}
+                  className={`h-1 w-full shrink-0 transition-colors duration-500 ease-in-out max-sm:min-w-20 ${isCompleted ? 'bg-green-800' : 'bg-gray-200'}`}
                 />
               )}
             </div>
