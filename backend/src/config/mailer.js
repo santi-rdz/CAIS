07dda565a17db4f05b61771c686789044ff4fd2b
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-06-03 00:59:14 -0700

    test: skip email sending when NODE_ENV is test

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -2,10 +2,13 @@ import { createTransport } from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const transporter = createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-})
+export const transporter =
+  process.env.NODE_ENV === 'test'
+    ? { sendMail: async () => ({ messageId: 'test-noop' }) }
+    : createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      })
